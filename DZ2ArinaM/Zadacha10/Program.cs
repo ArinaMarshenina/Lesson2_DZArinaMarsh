1c5f26b76016cfec311ca00752da0b86a3387b9b
Arina M <apelsinka7.ru@yandex.ru>
2022-09-24 22:36:25 +0300

    решение 10 задачи

---
+++ DZ2ArinaM/Zadacha10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введи трехзначное число ");
+int MyNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(MyNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
